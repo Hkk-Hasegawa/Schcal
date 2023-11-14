53f2f82143992eec37ee48d13887c4caa21b1caa
Yuuki <e-system@heiseikogyo.co.jp>
2023-11-14 16:54:59 +0900

    debug=false他

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6wms@o3@uy_7p&xtpufd0=xth0_x@jg&^j41k+z-w(4@67m2u3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['hkkhasegawa.pythonanywhere.com']
 
 
 # Application definition
