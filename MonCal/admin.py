f422d5a658ff7a28e8796991ebb1b8911c4d6f37
Yuuki <e-system@heiseikogyo.co.jp>
2023-11-28 15:38:54 +0900

    予約カレンダーの時刻選択を改変

--- MonCal/admin.py
+++ MonCal/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Schedule,Suresubject,EventSchedule,Event,Booking_time
+from .models import Schedule,Suresubject,EventSchedule,Event
 
 admin.site.register(Schedule)
 admin.site.register(Suresubject)
 admin.site.register(EventSchedule)
 admin.site.register(Event)
-admin.site.register(Booking_time)
+
 
